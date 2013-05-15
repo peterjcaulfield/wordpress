f92054480dd99c3d23b37e04ab60cb01e64d6605
peterjcaulfield <peterjcaulfield@gmail.com>
2013-05-15 22:11:57 +0100

    update to source

--- smart_meta_box.php
+++ smart_meta_box.php
@@ -67,50 +67,44 @@ class smart_meta_box
         }
     }
     
-    // render metabox function
-    public function show()
-    {
+    // Callback function to show fields in meta box
+
+    public function show($post) {
+
         // Use nonce for verification
-        echo '<input type="hidden" name="' . $this->id . '_meta_box_nonce" value="', wp_create_nonce('smartmetabox' . $this->id), '" />';
+        echo '<input type="hidden" name="' . $this->id . '_meta_box_nonce" value="', wp_create_nonce('smartmetabox' . $this->id) , '" />';
         echo '<table class="form-table">';
-        
         foreach ($this->meta_box['fields'] as $field) {
             extract($field);
-            
             $id = self::$prefix . $id;
-            
             $value = self::get($field['id']);
             if (empty($value) && !sizeof(self::get($field['id'], false))) {
                 $value = isset($field['default']) ? $default : '';
             }
-            
-            echo '<tr>', '<th style="width:20%"><label for="' . $id . '">' . $name . '</label></th>' . '<td>';
-            
-            include "fields/$type.php";
-            
+            echo '<tr>', '<th style="width:20%"><label for="', $id, '">', $name, '</label></th>', '<td>';
+            include "smart_meta_fields/$type.php";
             if (isset($desc)) {
-                echo ' <span>' . $desc . '</span>';
+                echo '&nbsp;<span class="description">' . $desc . '</span>';
             }
             echo '</td></tr>';
-        } // end foreach
-        
+        }
         echo '</table>';
-        
     }
-    
+
     // Save data from meta box
-    public function save($post_id)
-    {
+
+    public function save($post_id) {
+
         // verify nonce
         if (!isset($_POST[$this->id . '_meta_box_nonce']) || !wp_verify_nonce($_POST[$this->id . '_meta_box_nonce'], 'smartmetabox' . $this->id)) {
             return $post_id;
         }
-        
+
         // check autosave
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
             return $post_id;
         }
-        
+
         // check permissions
         if ('post' == $_POST['post_type']) {
             if (!current_user_can('edit_post', $post_id)) {
@@ -119,115 +113,39 @@ class smart_meta_box
         } elseif (!current_user_can('edit_page', $post_id)) {
             return $post_id;
         }
-        
         foreach ($this->meta_box['fields'] as $field) {
             $name = self::$prefix . $field['id'];
-            
+            $sanitize_callback = (isset($field['sanitize_callback'])) ? $field['sanitize_callback'] : '';
             if (isset($_POST[$name]) || isset($_FILES[$name])) {
                 $old = self::get($field['id'], true, $post_id);
                 $new = $_POST[$name];
-                
                 if ($new != $old) {
-                    self::set($field['id'], $new, $post_id);
+                    self::set($field['id'], $new, $post_id, $sanitize_callback);
                 }
             } elseif ($field['type'] == 'checkbox') {
-                /*
-                 * Checkboxes are not send in POST if they are not checked;
-                 * We should set them to 'false' instead of deleting them;
-                 * Otherwise, we won't be able to distinguish them from new fields, which have not been saved yet.
-                 */
-                self::set($field['id'], 'false', $post_id);
+                self::set($field['id'], 'false', $post_id, $sanitize_callback);
             } else {
                 self::delete($field['id'], $name);
             }
-        } // end foreach
+        }
     }
-    
-    static function get($name, $single = true, $post_id = null)
-    {
+    static function get($name, $single = true, $post_id = null) {
         global $post;
-        
         return get_post_meta(isset($post_id) ? $post_id : $post->ID, self::$prefix . $name, $single);
     }
-    
-    static function set($name, $new, $post_id = null)
-    {
+    static function set($name, $new, $post_id = null, $sanitize_callback = '') {
         global $post;
         
-        return update_post_meta(isset($post_id) ? $post_id : $post->ID, self::$prefix . $name, $new);
+        $id = (isset($post_id)) ? $post_id : $post->ID;
+        $meta_key = self::$prefix . $name;
+        $new = ($sanitize_callback != '' && is_callable($sanitize_callback)) ? call_user_func($sanitize_callback, $new, $meta_key, $id) : $new;
+        return update_post_meta($id, $meta_key, $new);
     }
-    
-    static function delete($name, $post_id = null)
-    {
+    static function delete($name, $post_id = null) {
         global $post;
-        
         return delete_post_meta(isset($post_id) ? $post_id : $post->ID, self::$prefix . $name);
     }
-    
+};
+function add_smart_meta_box($id, $opts) {
+    new SmartMetaBox($id, $opts);
 }
-
-// public function that calls SmartMetaBox constructor
-function add_meta_box($id, $opts)
-{
-    new smart_meta_box($id, $opts);
-}
-
-/*
- * Example of meta box generator usage
- * using all features supported by the generator
- */
-
-/*
-add_smart_meta_box('smart_meta_box_demo', array(
-'title'     => 'Smart meta box',
-'pages'        => array('post'),
-'context'   => 'normal',
-'priority'  => 'high',
-'fields'    => array(
-array(
-'name' => 'Text',
-'id' => 'smb_text',
-'default' => 'default',
-'desc' => 'Description',
-'type' => 'text',
-),
-array(
-'name' => 'Textarea',
-'id' => 'smb_textarea',
-'default' => 'default',
-'desc' => 'Description',
-'type' => 'textarea',
-),
-array(
-'name' => 'Checkbox',
-'id' => 'smb_checkbox',
-'default' => 'true',
-'desc' => 'Description',
-'type' => 'checkbox',
-),
-array(
-'name' => 'Select',
-'id' => 'smb_select',
-'default' => 'second-val',
-'desc' => 'Description',
-'type' => 'select',
-'options' => array(
-'first-val' => 'First',
-'second-val' => 'Second',
-)
-),
-array(
-'name' => 'Radios',
-'id' => 'smb_radio',
-'default' => 'second-val',
-'desc' => 'Description',
-'type' => 'radio',
-'options' => array(
-'first-val' => 'First',
-'second-val' => 'Second',
-)
-),
-) // closes fields array
-) // closes opts array
-); // closes function parameters 
-*/
