65f396f82adc8ba586006bcb545c695019a69c63
peterjcaulfield <peterjcaulfield@gmail.com>
2013-05-15 20:29:02 +0100

    function for obtaining a breadcrumb

---
+++ breadcrumb.php
@@ -0,0 +1,45 @@
+<?php
+/*
+* Function for grabbing breadcrumb to current page
+ */
+
+function get_breadcrumb($separator){
+	
+	global $post;
+	global $wp_query;
+	
+	$ids = array();
+	// add current post ID to id array
+	array_push($ids, $post->ID);
+	// get the parent post ID of the current post
+	$thisParentId = $post->post_parent;
+	// while the post parent id is not null, add to id's array and continue
+	while($thisParentId != 0){
+		array_push($ids, $thisParentId);
+		$parent = get_post($thisParentId);
+		$thisParentId = $parent->post_parent;
+	}
+
+    // flip array so we have current post first
+	$ids = array_reverse($ids);
+	// wrapper 
+	$breadcrumb = '<ul class="breadcrumb">';
+	
+	$listItems = array();
+	// mark up to li items and store in array
+	foreach($ids as $link){
+		$title = get_the_title($link);
+		$href = '<a href=' . get_permalink($link) . '>';
+		$string = '<li>' . $href . $title . '</a>' . '</li>';
+		array_push($listItems, $string);	
+	} 
+	// if no seperator specified, use a space
+	if($separator == NULL)
+	    $separator = ' ';
+	// convert to string using separator 
+	$concat = implode($listItems, $separator);
+    
+	$breadcrumb .= $concat . '</ul>';
+	
+	echo $breadcrumb;	
+}
